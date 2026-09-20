63e3019a2d8813f5e956e2f25bb701913fb19ece
Kledion2 <281180368+Kledion2@users.noreply.github.com>
2026-09-20 21:05:34 +0200

    attack spettacolo

--- src/attacks.h
+++ src/attacks.h
@@ -13,6 +13,36 @@ constexpr bool knightMoves(int from, int to) {
     diffRank = -diffRank;
   return (diffFile == 1 && diffRank == 2) || (diffFile == 2 && diffRank == 1);
 }
+
+constexpr bool kingMoves(int from, int to) {
+  if (from == to)
+    return false;
+  int diffFile = fileOf(from) - fileOf(to);
+  int diffRank = rankOf(from) - rankOf(to);
+  if (diffFile < 0)
+    diffFile = -diffFile;
+  if (diffRank < 0)
+    diffRank = -diffRank;
+  return (diffFile <= 1 && diffRank <= 1);
+}
+constexpr bool blackPawnMoves(int from, int to) {
+  if (from == to)
+    return false;
+  int diffFile = fileOf(from) - fileOf(to);
+  int diffRank = rankOf(from) - rankOf(to);
+  if (diffFile < 0)
+    diffFile = -diffFile;
+  return (diffFile == 1 && diffRank == 1);
+}
+constexpr bool whitePawnMoves(int from, int to) {
+  if (from == to)
+    return false;
+  int diffFile = fileOf(from) - fileOf(to);
+  int diffRank = rankOf(from) - rankOf(to);
+  if (diffFile < 0)
+    diffFile = -diffFile;
+  return (diffFile == 1 && diffRank == -1);
+}
 constexpr std::array<Bitboard, 64> knightTable() {
   std::array<Bitboard, 64> attacks{};
   for (int from = 0; from < 64; from++) {
@@ -25,17 +55,6 @@ constexpr std::array<Bitboard, 64> knightTable() {
 
   return attacks;
 }
-constexpr bool kingMoves(int from, int to) {
-  if (from == to)
-    return false;
-  int diffFile = fileOf(from) - fileOf(to);
-  int diffRank = rankOf(from) - rankOf(to);
-  if (diffFile < 0)
-    diffFile = -diffFile;
-  if (diffRank < 0)
-    diffRank = -diffRank;
-  return (diffFile <= 1 && diffRank <= 1);
-}
 constexpr std::array<Bitboard, 64> kingTable() {
   std::array<Bitboard, 64> attacks{};
   for (int from = 0; from < 64; from++) {
@@ -47,5 +66,30 @@ constexpr std::array<Bitboard, 64> kingTable() {
   }
   return attacks;
 }
+constexpr std::array<Bitboard, 64> blackPawnTable() {
+  std::array<Bitboard, 64> attacks{};
+  for (int from = 0; from < 64; from++) {
+    for (int to = 0; to < 64; to++) {
+      if (blackPawnMoves(from, to)) {
+        attacks[from] |= (1ULL << to);
+      }
+    }
+  }
+  return attacks;
+}
+constexpr std::array<Bitboard, 64> whitePawnTable() {
+  std::array<Bitboard, 64> attacks{};
+  for (int from = 0; from < 64; from++) {
+    for (int to = 0; to < 64; to++) {
+      if (whitePawnMoves(from, to)) {
+        attacks[from] |= (1ULL << to);
+      }
+    }
+  }
+  return attacks;
+}
+
 constexpr auto kingAttacks = kingTable();
 constexpr auto knightAttacks = knightTable();
+constexpr auto blackPawnAttacks = blackPawnTable();
+constexpr auto whitePawnAttacks = whitePawnTable();
