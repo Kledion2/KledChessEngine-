3aeb87ef594a4c1b24bdcc7da9e376b4e3631643
Kledion2 <281180368+Kledion2@users.noreply.github.com>
2026-09-20 20:40:15 +0200

    king attacks table done

--- src/attacks.h
+++ src/attacks.h
@@ -3,6 +3,8 @@
 #include <cmath>
 // attacchi cavallo generati a compile time
 constexpr bool knightMoves(int from, int to) {
+  if (from == to)
+    return false;
   int diffFile = fileOf(from) - fileOf(to);
   int diffRank = rankOf(from) - rankOf(to);
   if (diffFile < 0)
@@ -23,4 +25,27 @@ constexpr std::array<Bitboard, 64> knightTable() {
 
   return attacks;
 }
+constexpr bool kingMoves(int from, int to) {
+  if (from == to)
+    return false;
+  int diffFile = fileOf(from) - fileOf(to);
+  int diffRank = rankOf(from) - rankOf(to);
+  if (diffFile < 0)
+    diffFile = -diffFile;
+  if (diffRank < 0)
+    diffRank = -diffRank;
+  return (diffFile <= 1 && diffRank <= 1);
+}
+constexpr std::array<Bitboard, 64> kingTable() {
+  std::array<Bitboard, 64> attacks{};
+  for (int from = 0; from < 64; from++) {
+    for (int to = 0; to < 64; to++) {
+      if (kingMoves(from, to)) {
+        attacks[from] |= (1ULL << to);
+      }
+    }
+  }
+  return attacks;
+}
+constexpr auto kingAttacks = kingTable();
 constexpr auto knightAttacks = knightTable();
